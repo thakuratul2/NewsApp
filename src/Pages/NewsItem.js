40a79a919037825687b8650a20e14960ad38f679
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-09 21:26:43 +0530

    Card Design Add

--- src/Pages/NewsItem.js
+++ src/Pages/NewsItem.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function NewsItem() {
+function NewsItem(props) {
   return (
-    <div>NewsItem</div>
+    <div className="card" style={{width: "18rem"}}>
+  <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/12825/production/_128831857_bbc-sport-.png" className="card-img-top" alt=""/>
+  <div className="card-body">
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.desc}</p>
+    <a href="/newitem" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
   )
 }
 
--- src/components/News.js
+++ src/components/News.js
@@ -4,8 +4,20 @@ import NewsItem from '../Pages/NewsItem'
 function News() {
   return (
     <>
-    <div>News</div>
-    <NewsItem/>
+    <div className="container my-3">
+      <h2 className="text-center mb-5">News App - Top Heading</h2>
+      <div className="row">
+        <div className="col-md-4">
+          <NewsItem title="myTitle" desc="myDesc"/>
+        </div>
+        <div className="col-md-4">
+          <NewsItem title="myTitle" desc="myDesc"/>
+        </div>
+        <div className="col-md-4">
+          <NewsItem title="myTitle" desc="myDesc"/>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
